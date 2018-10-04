0442c2c8278481441263434a02acfcf231bbdc99
Wilson Barros <wbarroz@gmail.com>
2018-10-04 18:04:35 -0300

    Playing with tables

--- crawl.go
+++ crawl.go
@@ -7,16 +7,26 @@ import(
 )
 
 func main() {
-    c := colly.NewCollector()
+    c := colly.NewCollector(
+        colly.AllowedDomains("www.fundamentus.com.br",),
+    )
     var acronLink = regexp.MustCompile(`detalhes.*papel=[A-Z]{4}[0-9]{1,2}$`)
 
-    // Find and visit all links
+    // Find and visit all links, "first sweep":
     c.OnHTML("a", func(e *colly.HTMLElement) {
 
         if(acronLink.MatchString(e.Attr("href"))){
             e.Request.Visit(e.Attr("href"))
         }
     })
+
+    c.OnHTML("tr td:nth-of-type(1)", func(e *colly.HTMLElement) {
+            fmt.Println("First column of a table row:", e.Text)
+        })
+
+    c.OnHTML("tr td:nth-of-type(2)", func(e *colly.HTMLElement) {
+            fmt.Println("Second column of a table row:", e.Text)
+        })
 
     c.OnRequest(func(r *colly.Request) {
         fmt.Println("Visiting", r.URL)
